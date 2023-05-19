c3f7d94e80f7de9f44f85ac9415855b89d1bdf90
Edison Matias <devedisonmatias@gmail.com>
2023-05-18 23:08:37 -0300

    feat: add header

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include "liblist.h"
 
+void show_header()
+{
+    FILE *fp = fopen("header.txt", "r");
+
+    struct stat st;
+    stat("header.txt", &st);
+    char line[st.st_size];
+    
+    while (fgets(line, st.st_size, fp) != NULL)
+        printf("%s", line);
+
+    fclose(fp);
+}
+
 int main(int argc, char *argv[])
 {
+    show_header();
+
     list_t *list = create_list();
 
     for (int i = 1; i < argc; i++)
