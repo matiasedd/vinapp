d6229c98e17c7899d49f57d9b56e2fe503313bd0
Edison Matias <devedisonmatias@gmail.com>
2023-06-08 18:56:00 -0300

    feat: add remove option

--- src/main.c
+++ src/main.c
@@ -8,19 +8,30 @@ int main(int argc, char *argv[])
 
     int opt;
 
-    while ((opt = getopt(argc, argv, "ih")) != -1)
+    while ((opt = getopt(argc, argv, "ihr")) != -1)
     {
         switch (opt)
         {
         case 'i':
-            for (int i = 2; i < argc; i++)
+            for (int i = 3; i < argc; i++)
             {
-                printf("[%d/%d] %-32s", i - 1, argc - 2, argv[i]);
+                printf("[%d/%d] %-24s", i - 2, argc - 3, argv[i]);
                 
                 if (insert_member(argv[i], list) == SUCCESS)
-                    printf("DONE\n");
+                    printf("INSERTED\n");
                 else
                     printf("ERROR: file not found\n");
+            }
+            break;
+        case 'r':
+            for (int i = 3; i < argc; i++)
+            {
+                printf("[%d/%d] %-24s", i - 2, argc - 3, argv[i]);
+                
+                if (remove_member(argv[i], list) == SUCCESS)
+                    printf("REMOVED\n");
+                else
+                    printf("ERROR: something went wrong\n");
             }
             break;
         case 'h':
